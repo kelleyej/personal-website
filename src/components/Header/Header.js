85570617f73fe37ac99092d749f0eaf221e77754
Erin Kelley <erin.kelley01@gmail.com>
2024-05-23 15:01:09 -0600

    Add tech stack icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,24 @@
 import './Header.css';
 import Github from '../../assets/github.png';
 import Linkedin from '../../assets/linkedin.png'
+import Scenic from '../../assets/mountainscenic.png'
+
 
 export default function Header(){
     return (
         <header >
-            <h1>Erin Kelley</h1>
+            <div className='intro'>
+              <h1>Erin Kelley</h1>
+            <img className='scenic' src={Scenic} />
+            {/* <p className='role'>Software Developer.</p>   */}
+            </div>
+            
             {/* <div className='media-container'>
                 <a href='https://github.com/kelleyej'><img className='github' src={Github} /></a>
                 <a href='https://www.linkedin.com/in/kelleyej/'><img className='linkedin' src={Linkedin} /></a> 
             </div>  */}
             <div className='links'>
+                
                 <a href='#bio-container'>About Me</a>
               <a>Resume</a>
             <a href='#project-container'>Projects</a>  
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,6 +8,12 @@ import { useState } from 'react';
 import Cypress from '../../assets/cypress.png'
 import ReactIcon from '../../assets/react.png';
 import House from '../../assets/house.png'
+import Express from '../../assets/express.png';
+import Javascript from '../../assets/javascript.png';
+import HTML from '../../assets/html.png'
+import CSS from '../../assets/css.png';
+import Postgresql from '../../assets/postgresql.png';
+import Knex from '../../assets/knex.png'
 
 export default function Projects(){
 const [mouseEvent, setMouseEvent] = useState(false)
@@ -15,24 +21,29 @@ console.log(mouseEvent)
     return (
         <section id='project-container'>
             <div className='project-title'>
-                <h3>Projects</h3>
-            <img className='icon' src={Icon} />
+                <h3>Projects.</h3>
+            {/* <img className='icon' src={Icon} /> */}
             </div>
             
             <section className='project-grid'>
-                <div className='project' onMouseEnter={() => setMouseEvent(true)} onMouseLeave={() => setMouseEvent(false)}>
+                <div>
                    <p>PINE</p>
                    <a href='https://pine-project.vercel.app/'><img className='link' src={Link} /></a>
                    <a href='https://github.com/kelleyej/pine-project'><img className='link' src={Github} /></a>
-                {!mouseEvent ? <img className='pine' src={Pine} />  : <div><img className='cypress' src={Cypress} /><img className='cypress' src={ReactIcon} /><p>A guide to all 63 National Parks. Explore the different regions and states and track which National Parks you have visited!</p></div>}
+                   <div className='social'><img className='cypress' src={Cypress} /><img className='react' src={ReactIcon}/><img src={Express} className='express'/> </div>
+                 <img className='pine' src={Pine} />  
                 </div>
-               <div className='project' onMouseEnter={() => setMouseEvent(true)} onMouseLeave={() => setMouseEvent(false)}>
+               <div>
                 <p>Travel Tracker</p>
                 <a href='https://pine-project.vercel.app/'><img className='link' src={Github} /></a>
+                <div className='social'><img className='cypress' src={Javascript} /><img className='cypress' src={HTML} /><img className='cypress' src={CSS} /></div>
                 <img className='travel' src={Travel} />
                </div>
-               <div className='project' onMouseEnter={() => setMouseEvent(true)} onMouseLeave={() => setMouseEvent(false)}>
+               <div>
                 <p>Housemate Hunter</p>
+                    <a href='https://housemate-hunter.vercel.app/'><img className='link' src={Link} /></a>
+                   <a href='https://github.com/kelleyej/pine-project'><img className='link' src={Github} /></a>
+                   <div className='social'><img className='cypress' src={Cypress} /><img className='react' src={ReactIcon} /><img src={Express} className='express'/><img className='cypress' src={Postgresql} /><img className='cypress' src={Knex} /></div>
                 <img className='house'src={House} />
                 </div>
                 <p>Brain Food</p>
