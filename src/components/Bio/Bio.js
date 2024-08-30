33f5409db29399feb96535758122de89ce986b2a
Erin Kelley <erin.kelley01@gmail.com>
2024-08-30 08:31:20 -0600

    Change google docs resume url

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -6,32 +6,32 @@ import Gmail from '../../assets/gmail.png';
 import { motion } from 'framer-motion';
 import CV from '../../assets/cv.png'
 
-export default function Bio(){
+export default function Bio() {
     return (
         <main id='bio-container'>
             <section className='bio'>
-                     <div className='media-container'>
-                <motion.a transition={{delay: 0.3}} initial={{scale: 0}} animate={{scale:1}}target="_blank" href='https://github.com/kelleyej'><img  className='github' src={Github} /></motion.a>
-                <motion.a transition={{delay: 0.45}} initial={{scale: 0}} animate={{scale:1}} target="_blank" href='https://www.linkedin.com/in/kelleyej/'><img className='linkedin' src={Linkedin} /></motion.a> 
-                <motion.a transition={{delay: 0.6}} initial={{scale: 0}} animate={{scale:1}} href="mailto:erin.kelley01@gmail.com"><img className='linkedin' src={Gmail} /></motion.a>
-                <motion.a transition={{delay: 0.75}} initial={{scale: 0}} animate={{scale:1}} target="_blank" href='https://docs.google.com/document/d/1ObhAmSSIMxJcBMwDzsPG6LYI_XB0d7rL9OBp2dvxT3Y/edit?usp=sharing' ><img className='linkedin' src={CV} /></motion.a>
-            </div>
+                <div className='media-container'>
+                    <motion.a transition={{ delay: 0.3 }} initial={{ scale: 0 }} animate={{ scale: 1 }} target="_blank" href='https://github.com/kelleyej'><img className='github' src={Github} /></motion.a>
+                    <motion.a transition={{ delay: 0.45 }} initial={{ scale: 0 }} animate={{ scale: 1 }} target="_blank" href='https://www.linkedin.com/in/kelleyej/'><img className='linkedin' src={Linkedin} /></motion.a>
+                    <motion.a transition={{ delay: 0.6 }} initial={{ scale: 0 }} animate={{ scale: 1 }} href="mailto:erin.kelley01@gmail.com"><img className='linkedin' src={Gmail} /></motion.a>
+                    <motion.a transition={{ delay: 0.75 }} initial={{ scale: 0 }} animate={{ scale: 1 }} target="_blank" href='https://docs.google.com/document/d/1Wn8kO_f-g_SnJl-KgYM0cKrXvFhrktDA3QTPMWBNRBQ/edit?usp=sharing' ><img className='linkedin' src={CV} /></motion.a>
+                </div>
                 <div className='style'>
-                   <motion.img initial={{ y: -1000 }} animate={{ y: 0 }} transition={{ duration: 2, type: "spring", stiffness: 100, damping: 12 }} className='photo' src={Photo} />
-                <motion.h2 transition={{delay: 0.4}} initial={{scale: 0}} animate={{scale:1}} className='style'>
-               Hello, there!
-                 </motion.h2>
-                <p className='bio-message'>After seven years as a clinical Audiologist, I turned my passion for <span>problem solving</span> and use of <span>technology</span> to improve quality of life into a career in <span>software development</span> -- particularly to continue contributing to positive user outcomes while fostering my desire for lifelong <span>learning</span>. Being outside makes me the happiest and you can often find me in the mountains, cycling, hiking, or camping. I have a cat named Squash, who has soaked in all the knowledge sitting on my lap when I'm at the computer. 
-                </p> 
-                <br>
-                </br>
-                <p className='alliteration'>I love <span className='c'>challenges</span>, <span className='c'>creativity</span>, and <span className='c'>collaboration</span> <span className='italic'>(and alliteration)</span>.</p>
-               </div>
-                
+                    <motion.img initial={{ y: -1000 }} animate={{ y: 0 }} transition={{ duration: 2, type: "spring", stiffness: 100, damping: 12 }} className='photo' src={Photo} />
+                    <motion.h2 transition={{ delay: 0.4 }} initial={{ scale: 0 }} animate={{ scale: 1 }} className='style'>
+                        Hello, there!
+                    </motion.h2>
+                    <p className='bio-message'>After seven years as a clinical Audiologist, I turned my passion for <span>problem solving</span> and use of <span>technology</span> to improve quality of life into a career in <span>software development</span> -- particularly to continue contributing to positive user outcomes while fostering my desire for lifelong <span>learning</span>. Being outside makes me the happiest and you can often find me in the mountains, cycling, hiking, or camping. I have a cat named Squash, who has soaked in all the knowledge sitting on my lap when I'm at the computer.
+                    </p>
+                    <br>
+                    </br>
+                    <p className='alliteration'>I love <span className='c'>challenges</span>, <span className='c'>creativity</span>, and <span className='c'>collaboration</span> <span className='italic'>(and alliteration)</span>.</p>
+                </div>
+
             </section>
             <section className='container'>
                 <img className='mountain' />
-            
+
             </section>
         </main>
     )
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,32 +5,30 @@ import Github from '../../assets/github.png';
 import Linkedin from '../../assets/linkedin.png';
 import Gmail from '../../assets/gmail.png';
 
-export default function Footer(){
+export default function Footer() {
     return (
         <footer id='footer'>
             <div class='test'>
-                <img className='pic'src={Pic} />
-              <div className='social-container'>
-                   <a target="_blank" href='https://github.com/kelleyej'><img className='github-small' src={Github} /></a>
-                <a target="_blank" href='https://www.linkedin.com/in/kelleyej/'><img className='linkedin-small' src={Linkedin} /></a> 
-                <a href="mailto:erin.kelley01@gmail.com"><img className='small' src={Gmail} /></a>
-             </div>
-             <a className='resume' target="_blank" href='https://docs.google.com/document/d/1ObhAmSSIMxJcBMwDzsPG6LYI_XB0d7rL9OBp2dvxT3Y/edit?usp=sharing'>Resume</a>
+                <img className='pic' src={Pic} />
+                <div className='social-container'>
+                    <a target="_blank" href='https://github.com/kelleyej'><img className='github-small' src={Github} /></a>
+                    <a target="_blank" href='https://www.linkedin.com/in/kelleyej/'><img className='linkedin-small' src={Linkedin} /></a>
+                    <a href="mailto:erin.kelley01@gmail.com"><img className='small' src={Gmail} /></a>
+                </div>
+                <a className='resume' target="_blank" href='https://docs.google.com/document/d/1Wn8kO_f-g_SnJl-KgYM0cKrXvFhrktDA3QTPMWBNRBQ/edit?usp=sharing'>Resume</a>
             </div>
             <div className='message'>
                 <h4>Say, hi!</h4>
-               <a href="mailto:erin.kelley01@gmail.com" ><p className='email'>erin.kelley01@gmail.com</p></a>
-            
-            <p className='greeting'>I would love to hear from you. Feel free to reach out.</p> 
+                <a href="mailto:erin.kelley01@gmail.com" ><p className='email'>erin.kelley01@gmail.com</p></a>
+
+                <p className='greeting'>I would love to hear from you. Feel free to reach out.</p>
             </div>
-            
+
             <div className='bottom'>
 
-              <img className='range' src={Range} />  
+                <img className='range' src={Range} />
             </div>
-            
-            
         </footer>
-        
+
     )
 }
